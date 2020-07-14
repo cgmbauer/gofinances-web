d14ba96ff0dad5ae69a46ff5edd7c77678c57f28
cgmbauer <cgmbauer@gmail.com>
2020-07-13 23:27:28 -0300

    format value to R$ BRL

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,6 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number, type?: 'income' | 'outcome'): string =>
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+    value,
+  );
 
 export default formatValue;
